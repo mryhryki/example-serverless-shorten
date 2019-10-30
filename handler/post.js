ab81cbb9f830bec9efa095329c153676ae585802
hyiromori <hyiromori@gmail.com>
2019-10-30 13:54:24 +0900

    URLを短くした。

--- handler/post.js
+++ handler/post.js
@@ -1,10 +1,11 @@
-const { v4: uuid } = require('uuid');
+const crypto = require('crypto');
 const { putItem } = require('./lib/dynamodb');
 
 module.exports.handler = async (event) => {
   const { Origin } = event.headers;
-  const { url } = JSON.parse(event.body);
-  const key = uuid();
+  const json = JSON.parse(event.body);
+  const { url } = json;
+  const key = crypto.randomBytes(10).toString('base64').replace(/[+/]/g, '').substr(0, 5);
   await putItem('shorten_url', key, { url });
   const payload = {
     url: `${Origin}/${key}`,
