35922485c5159b139421b7d0fa9df55d175f63a5
Hiroyuki Moriya <hyiromori@gmail.com>
2019-11-29 14:31:11 +0900

    README 書いた (#1)

--- handler/index.js
+++ handler/index.js
@@ -3,7 +3,7 @@ const html = `
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <title>Shorten</title>
+  <title>URL Shortener</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
